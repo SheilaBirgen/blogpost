944d845d47912f3262cf8ee3f9ac9452bb34da86
SheilaBirgen <jeronobergen@gmail.com>
2020-02-10 15:39:21 +0300

    Add class updateprofile to forms.py

--- app/main/route.py
+++ app/main/route.py
@@ -1,5 +1,5 @@
 from flask import render_template,request,Blueprint
-from app.models import app
+from pitcher.models import app
  
 
 main = Blueprint("main",__name__)
@@ -8,7 +8,7 @@ main = Blueprint("main",__name__)
 @main.route("/")
 @main.route("/home")
 def home():
-    posts = App.query.all()
+    posts = Pitch.query.all()
     return render_template("home.html",posts.posts,title="Home page")
     
        
