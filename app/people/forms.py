b79afbda01eb241d1e33f1460fb9c7a71af62b03
SheilaBirgen <jeronobergen@gmail.com>
2020-02-10 17:27:10 +0300

    add validation for username to forms.py in people

--- app/people/forms.py
+++ app/people/forms.py
@@ -2,12 +2,18 @@ from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from flask_wtf.file import FileAllowed
-from .models import User
+from app.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password", validators=[
-                                     DataRequired(), EqualTo("password")])
+    confirm_password = PasswordField("Confirm Password", validators=[ DataRequired(), EqualTo("password")])
     submit = SubmitField("Sign Up")
+
+def validate_username(self,username):
+     # if username.data != current_user.username:
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                "That username is already taken. Please pick another one")
