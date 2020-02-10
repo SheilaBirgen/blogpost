3471d3792aa85e0319a44bd38cd5bad7b81506ef
SheilaBirgen <jeronobergen@gmail.com>
2020-02-10 16:19:53 +0300

    add class user and pitch to models.py

--- app/posts/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,TextAreaField
-from wtforms.validators import Required
-
-class UpdateProfile(FlaskForm):
-    bio = TextAreaField('tell us about you.',validators= [Required()])
-    submit = SubmitField('Submit')git 
