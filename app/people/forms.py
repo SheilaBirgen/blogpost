0be583f90aa7ba3fd188ba4e93fe582a1439973c
SheilaBirgen <jeronobergen@gmail.com>
2020-02-11 18:50:31 +0300

    add about.html

--- app/people/forms.py
+++ app/people/forms.py
@@ -53,3 +53,20 @@ class UpdateAccountForm(FlaskForm):
             if user:
                 raise ValidationError(
                     "That email  is already taken. Please pick another one")
+
+class RequestResetForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    submit = SubmitField("Request Password Reset")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is None:
+            raise ValidationError(
+                "There is no account with email you must register first")
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Password", validators=[DataRequired(), EqualTo("password")])
+    submit = SubmitField("Reset Password")
