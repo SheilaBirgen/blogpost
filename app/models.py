a34809b43e9883603953ae329bbbc65367567127
SheilaBirgen <jeronobergen@gmail.com>
2020-02-12 14:58:46 +0300

    Add class vote to models.py

--- app/models.py
+++ app/models.py
@@ -10,35 +10,48 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    email = db.Column(db.String(20), nullable=False, unique=True)
-    img_file = db.Column(db.String(20), default="default.jpeg")
-    password = db.Column(db.String(10),  nullable=False)
-    posts = db.relationship("Pitch", backref="author", lazy=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
 
-    # def get_reset_token(self, expires_sec=1800):
-    #     s = Serializer(app.config['SECRET_KEY'], expires_sec)
-    #     return s.dumps({'user_id': self.id}).decode('utf-8')
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.img_file}')"
 
         
 class Pitch(db.Model):
+    __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
-
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
     def __repr__(self):
-        return f"Pitch({self.title}, {self.date_posted}, {self.img_file})"
+        return f"Post('{self.title}', '{self.date_posted}')"
 
 class Comment(db.Model):
+   __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key=True)
-    date_posted = db.Column(db.DateTime, nullable=False,default=datetime.utcnow)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-    
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
     def __repr__(self):
-        return f"User('{self.date_posted}')"
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+class Vote(db.Model):
+    __tablename__ = 'votes'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    upvote = db.Column('upvote',db.Integer,default=0)
+	downvote = db.Column('downvote',db.Integer,default=0)
+
+    def __repr__(self):
+        return f"User('{self.votes}')"
